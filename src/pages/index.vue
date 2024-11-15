55c3e8213eab12224a65b4a0aae1916422ed6ea9
lpp <geolifestudy@gmail.com>
2024-11-15 09:03:22 +0800

    onnx

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <!-- <div grid-head-center-foot> -->
   <div full-flex flex-col>
     <SysHeader h-14 bg-deep-1 z-11 shadow />
-    <!-- :style="'clip-path: polygon(0 0, 100% 0, 100% 100%, 0 '+clipPath[0]+'%)'"  -->
     <main grow bg-deep-1 px-1 py-2>
       <router-view></router-view>
     </main>
@@ -10,17 +8,5 @@
   </div>
 </template>
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { SysSettingStore } from '@/stores/sys'
-import router from '@/router';
-const { sysStyle } = storeToRefs(SysSettingStore())
-sysStyle.value.headFootShow = true
-const dragRef = ref();
-const addPage = () => {
-  // 拖拽中，不执行后续操作
-  if (dragRef.value.$el.isMoving) return
-  // 导航到 /blog/blog-new
-  router.push('/blog/blog-new')
-}
 </script>
 <style scoped></style>
--- src/pages/index/component_lib/libBase/onnxruntime.vue
+++ src/pages/index/component_lib/libBase/onnxruntime.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>todo</div>
+    <div>todo</div>
 </template>
- <script setup lang='ts'>
+<script setup lang='ts'>
 /**
  * @version      :0.0
  * @author       :itild
@@ -17,18 +17,38 @@
  * 6 watch 7 fuc 8 fetch 9 code_block
  */
 ////////////////////1_import_______////////////////////
+import * as ort from 'onnxruntime-web';
+// data/linear_regression_fixed.onnx
+onMounted(async () => {
+    const session = await ort.InferenceSession.create('data/linear_regression_fixed.onnx');
+    const input = new ort.Tensor('float32', [5.0]);
+    // 获取输入和输出的名称
+    const input_name = session.inputNames[0];
+    const output_name = session.outputNames[0];
+
+    // 创建输入张量
+    const input_tensor = new ort.Tensor('float32', input_data, [1, input_data.length]);
+
+    // 进行预测
+    const result = await session.run({ [input_name]: input_tensor });
+
+    // 获取预测结果
+    const output_data = result[output_name].data;
+
+    console.log(output_data); // 输出预测结果
+})
+
 ////////////////////2_type_________////////////////////
 ////////////////////3_class________////////////////////
 ////////////////////4_obj__________////////////////////
 ////////////////////5_vue__________////////////////////
 onMounted(() => {
-  //console.log('onMounted_debugger')
- })
+    //console.log('onMounted_debugger')
+})
 ////////////////////6_watch________////////////////////
 ////////////////////7_fuc__________////////////////////
 ////////////////////8_fetch________////////////////////
 ////////////////////9_code_block___////////////////////
 ////////////////////_______________////////////////////
 </script>
-<style scoped>
-</style>
+<style scoped></style>
